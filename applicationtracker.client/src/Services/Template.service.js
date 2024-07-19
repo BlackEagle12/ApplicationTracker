14a6450ab09b5a7d499f80cc350f452218032c3e
krupspurohit <krupapurohit99@gmail.com>
2024-07-20 00:48:16 +0530

    qwertyuiop

--- applicationtracker.client/src/Services/Template.service.js
+++ applicationtracker.client/src/Services/Template.service.js
@@ -2,9 +2,18 @@ import EndpointConstants from "../HttpModule/Endpoints";
 import HttpModule from "../HttpModule/HttpModule";
 
 export const addEmailTemplate = async (data) => {
-	return HttpModule.post(EndpointConstants.addEmailTemplate, data, {
-		headers: {
-			"content-type": "",
-		},
-	});
+
+	var formData = new FormData();
+	formData.append('UserId', 1);
+	formData.append('TemplateName', data.templateName);
+	formData.append('Subject', data.subject);
+	formData.append('Body', data.body);
+	formData.append('IsHtml', data.isHtml);
+
+	console.log(formData);
+	// return HttpModule.post(EndpointConstants.addEmailTemplate, data, {
+	// 	headers: {
+	// 		"content-type": "multipart/form-data",
+	// 	},
+	// });
 };
